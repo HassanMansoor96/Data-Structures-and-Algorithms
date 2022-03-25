1191123d0cd496420cafc5300de16d4686af2dd0
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-03-25 10:01:18 +0200

    chapter5

--- Classwork/Chapter5/MyQue.js
+++ Classwork/Chapter5/MyQue.js
@@ -1,48 +1,88 @@
-var dancers = [
-    'F Allison McMillan',
-'M Frank Opitz',
-'M Mason McMillan',
-'M Clayton Ruff',
-'F Cheryl Ferenback',
-'M Raymond Williams',
-'F Jennifer Ingram',
-'M Bryan Frazer',
-'M David Durr',
-'M Danny Martin',
-'F Aurora Adney'
-]
-
-
-function Dancer(name, sex) {
-    this.name = name;
-    this.sex = sex;
+class Queue {
+    constructor() {
+        this.dataStore = [];
+        this.enqueue = enqueue;
+        this.dequeue = dequeue;
+        this.front = front;
+        this.back = back;
+        this.toString = toString;
+        this.empty = empty;
+    }
 }
 
-function getDancers(males, females) {
-    var names = read("dancers.txt").split("\n");
-    for (var i = 0; i < names.length; ++i) {
-        names[i] = names[i].trim();
+function enqueue(element) {
+    this.dataStore.push(element);
+}
+function dequeue() {
+    return this.dataStore.shift();
+}
+function front() {
+    return this.dataStore[0];
+}
+function back() {
+    return this.dataStore[this.dataStore.length - 1];
+}
+function toString() {
+    var retStr = "";
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        retStr += this.dataStore[i] + "\n";
     }
-    for (var i = 0; i < names.length; ++i) {
-        var dancer = names[i].split(" ");
-        var sex = dancer[0];
-        var name = dancer[1];
+    return retStr;
+}
+function empty() {
+    if (this.dataStore.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+var dancers = [
+    "F Allison McMillan",
+    "M Frank Opitz",
+    "M Mason McMillan",
+    "M Clayton Ruff",
+    "F Cheryl Ferenback",
+    "M Raymond Williams",
+    "F Jennifer Ingram",
+    "M Bryan Frazer",
+    "M David Durr",
+    "M Danny Martin",
+    "F Aurora Adney"
+]
+
+class Dancer {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+}
+
+var males = new Queue();
+var females = new Queue();
+
+function getDancers() {
+    var names = [];
+    for (var idx = 0; idx < dancers.length; idx++) {
+        var person = dancers[idx].split(" ")
+        var gender = person[0];
+        var firstName = person[1];
+        var lastName = person[2];
+
+        names[idx] = firstName + " " + lastName;
+    }
+
+    for (var i = 0; i < dancers.length; ++i) {
+        var person = dancers[i].split(" ");
+        var sex = person[0];
+        var name = names[i];
         if (sex == "F") {
-            females.enqueue(new Dancer(name, sex));
+            var dancer = new Dancer(name, sex);
+            females.enqueue(dancer);
         }
         else {
             males.enqueue(new Dancer(name, sex));
         }
     }
-}
-
-function dance(males, females) {
-    console.log("The dance partners are: \n");
-    while (!females.empty() && !males.empty()) {
-        person = females.dequeue();
-        putstr("Female dancer is: " + person.name);
-        person = males.dequeue();
-        console.log(" and the male dancer is: " + person.name);
-    }
-    console.log();
 }
