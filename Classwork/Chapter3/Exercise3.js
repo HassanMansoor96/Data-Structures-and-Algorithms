6d8446958d489daf04c14c2c60ede5bd900f6759
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-03-28 15:10:59 +0200

    exercise3

--- Classwork/Chapter3/Exercise3.js
+++ Classwork/Chapter3/Exercise3.js
@@ -3,9 +3,82 @@ is larger than any of the elements currently in the list. Larger can mean either
 than when working with numeric values, or further down in the alphabet, when
 working with textual values.*/
 
+function List() {
+    this.array = [1, 37, 8, 50, "a", "o", "ab"];
+    this.add = add;
+    this.check = check;
+    this.display = display;
+}
+
+function display(data, description) {
+    console.log(data, "---" + description + "---");
+}
+
+function add(data) {
+    if (this.check(data)) {
+        this.array.push(data);
+        this.display(this.array, "succses")
+    }
+    else {
+        this.display(this.array, "error")
+    }
+}
+
+function check(data) {
+    var arrayType = this.array.filter(function (number) {
+        return typeof number == typeof data;
+    });
+    function bigger(number) {
+        return data > number;
+    }
+    return arrayType.every(bigger);
+}
+
+var list = new List();
+list.add(100); // Will add to the list and show succses
+list.add("m") // Will not add to the list and show error
+
+
+
 
 /*2. Write a function that inserts an element into a list only if the element to be inserted
 is smaller than any of the elements currently in the list.*/
+
+function List() {
+    this.array = [1, 37, 8, 50, "a", "o", "ab"];
+    this.add = add;
+    this.check = check;
+    this.display = display;
+}
+
+function display(data, description) {
+    console.log(data, "---" + description + "---");
+}
+
+function add(data) {
+    if (this.check(data)) {
+        this.array.push(data);
+        this.display(this.array, "succses")
+    }
+    else {
+        this.display(this.array, "error")
+    }
+}
+
+function check(data) {
+    var arrayType = this.array.filter(function (number) {
+        return typeof number == typeof data;
+    });
+    function smaller(number) {
+        return data < number;
+    }
+    return arrayType.every(smaller);
+}
+
+var list = new List();
+list.add(0); // Will add to the list and show succses
+list.add("l") // Will not add to the list and show error
+
 
 
 
