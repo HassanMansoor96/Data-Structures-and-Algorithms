ea8128148c95dbc32183bcfe14302aadeffcdb6a
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-03-30 13:19:20 +0200

    binary

--- Classwork/Chapter10/binarySearch.js
+++ Classwork/Chapter10/binarySearch.js
@@ -66,7 +66,7 @@ class BST {
         return null;
       }
     }
-    return current;
+    return current.data;
   }
   isPresent(data) {
     let current = this.root;
@@ -237,4 +237,8 @@ function run() {
   console.log("postOrder: " + bst.postOrder());
 
   console.log("levelOrder: " + bst.levelOrder());
+  console.log("Find Member: " + bst.find(22));
+  console.log("Find Min: " + bst.findMin());
+  console.log("Find Max: " + bst.findMax());
+  console.log("Remove Member: " + bst.remove(20))
 }
