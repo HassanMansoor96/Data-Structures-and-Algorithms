9d49df466d3f9ddc2e56742e1e3a5c217768bc5c
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-04-05 12:13:23 +0200

    exercises

--- Classwork/Chapter5/Exercise5.js
+++ Classwork/Chapter5/Exercise5.js
@@ -2,7 +2,54 @@
 that allows elements to be added and removed from both the front and the back of
 the list. Test your class in a program.*/
 
-
+function Queue() {
+    this.dataStore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+}
+function enqueue(element) {
+    this.dataStore.push(element);
+}
+function dequeue() {
+    return this.dataStore.shift();
+}
+function front() {
+    return this.dataStore[0];
+}
+function back() {
+    return this.dataStore[this.dataStore.length - 1];
+}
+function toString() {
+    var retStr = "";
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        retStr += this.dataStore[i] + "\n";
+    }
+    return retStr;
+}
+function empty() {
+    if (this.dataStore.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function run() {
+    var q = new Queue();
+    q.enqueue("Meredith");
+    q.enqueue("Cynthia");
+    q.enqueue("Jennifer");
+    console.log(q.toString());
+    q.dequeue();
+    console.log(q.toString());
+    console.log("Front of queue: " + q.front());
+    console.log("Back of queue: " + q.back());
+}
+run();
 
 /*2. Use the Deque class you created in Example 5-1 to determine if a given word is a
 palindrome.*/
@@ -13,7 +60,83 @@ palindrome.*/
 elements have higher numbers rather than lower numbers. Test your implementation
 with the example in the chapter.*/
 
+function Queue() {
+    this.dataStore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+}
+function enqueue(element) {
+    this.dataStore.push(element);
+}
 
+function front() {
+    return this.dataStore[0];
+}
+function back() {
+    return this.dataStore[this.dataStore.length - 1];
+}
+
+function empty() {
+    if (this.dataStore.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function Patient(name, code) {
+    this.name = name;
+    this.code = code;
+}
+function dequeue() {
+    var priority = this.dataStore[0].code;
+    for (var i = 1; i < this.dataStore.length; ++i) {
+        if (this.dataStore[i].code < priority) {
+            priority = i;
+        }
+    }
+    return this.dataStore.splice(priority, 1);
+}
+function toString() {
+    var retStr = "";
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        retStr += this.dataStore[i].name + " code: "
+            + this.dataStore[i].code + "\n";
+    }
+    return retStr;
+}
+function run(){
+var p = new Patient("Smith", 5);
+var ed = new Queue();
+ed.enqueue(p);
+p = new Patient("Jones", 4);
+ed.enqueue(p);
+p = new Patient("Fehrenbach", 6);
+ed.enqueue(p);
+p = new Patient("Brown", 1);
+ed.enqueue(p);
+p = new Patient("Ingram", 1);
+ed.enqueue(p);
+console.log(ed.toString());
+var seen = ed.dequeue();
+console.log("Patient being treated: " + seen[0].name);
+console.log("Patients waiting to be seen: ")
+console.log(ed.toString());
+// another round
+var seen = ed.dequeue();
+console.log("Patient being treated: " + seen[0].name);
+console.log("Patients waiting to be seen: ")
+console.log(ed.toString());
+var seen = ed.dequeue();
+console.log("Patient being treated: " + seen[0].name);
+console.log("Patients waiting to be seen: ")
+console.log(ed.toString());
+}
+run();
 
 /*4. Modify the ED example (Example 5-5) so the user can control the activity in the
 ED. Create a menu system that allows the user to choose from the following activities:
--- Classwork/Chapter6/Exercise6.js
+++ Classwork/Chapter6/Exercise6.js
@@ -1,6 +1,17 @@
 /*1. Implement the advance(n) function so that when executed, the current node is
 moved n nodes forward in the list.*/
 
+function Node(element) {
+    this.element = element;
+    this.next = null;
+    }
+    function LList() {
+        this.head = new Node("head");
+        this.find = find;
+        this.insert = insert;
+        this.remove = remove;
+        this.display = display;
+        }
 
 /*2. Implement the back(n) function so that when executed, the current node is moved
 n spaces backward in the list.*/
@@ -19,7 +30,98 @@ entered interactively into the program.*/
 
 /*5. Rewrite your solution to Example 6-4 using a doubly linked list.*/
 
+function Node(element) {
+    this.element = element;
+    this.next = null;
+    this.previous = null;
+}
 
+function LList() {
+    this.head = new Node("head");
+    this.find = find;
+    this.insert = insert;
+    this.display = display;
+    this.remove = remove;
+    this.findLast = findLast;
+    this.dispReverse = dispReverse;
+}
+
+function dispReverse() {
+    var currNode = this.head;
+    currNode = this.findLast();
+    while (!(currNode.previous == null)) {
+        console.log(currNode.element);
+        currNode = currNode.previous;
+    }
+}
+
+function findLast() {
+    var currNode = this.head;
+    while (!(currNode.next == null)) {
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+function remove(item) {
+    var currNode = this.find(item);
+    if (!(currNode.next == null)) {
+        currNode.previous.next = currNode.next;
+        currNode.next.previous = currNode.previous;
+        currNode.next = null;
+        currNode.previous = null;
+    }
+}
+
+// findPrevious is no longer needed
+/*function findPrevious(item) {
+var currNode = this.head;
+while (!(currNode.next == null) &&
+(currNode.next.element != item)) {
+currNode = currNode.next;
+}
+return currNode;
+}*/
+
+function display() {
+    var currNode = this.head;
+    while (!(currNode.next == null)) {
+        console.log(currNode.next.element);
+        currNode = currNode.next;
+    }
+}
+
+function find(item) {
+    var currNode = this.head;
+    while (currNode.element != item) {
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+function insert(newElement, item) {
+    var newNode = new Node(newElement);
+    var current = this.find(item);
+    newNode.next = current.next;
+    newNode.previous = current;
+    current.next = newNode;
+}
+function run(){
+var cities = new LList();
+
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Carlisle", "Russellville");
+cities.insert("Alma", "Carlisle");
+
+cities.display();
+console.log();
+cities.remove("Carlisle");
+cities.display();
+console.log();
+cities.dispReverse();
+}
+run();
 
 /*6. According to legend, the first-century Jewish historian Flavius Josephus was about
 to be captured along with a band of 40 compatriots by Roman soldiers during the
